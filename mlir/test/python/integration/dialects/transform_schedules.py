42669e908f3547ad617fd65ae84619985e6c9a92
Martin Lücke <martin.luecke@ed.ac.uk>
2024-01-15 10:39:07 +0100

    iterate on schedules

--- mlir/test/python/integration/dialects/transform_schedules.py
+++ mlir/test/python/integration/dialects/transform_schedules.py
@@ -14,10 +14,12 @@ from mlir.dialects.transform.structured import *
 from mlir.dialects.transform.loop import loop_unroll
 from mlir.dialects.transform.extras import named_sequence, apply_patterns
 from mlir.extras import types as T
+from mlir.extras.types import memref, tensor
 from mlir.dialects.builtin import module, ModuleOp
+from mlir.dialects.transform.extras import *
 
 
-def construct_and_print_in_module(f: Callable[[ModuleOp], None]):
+def construct_and_print_in_module(f: Callable[[Module], None]):
     print("\nTEST:", f.__name__)
     with Context(), Location.unknown():
         module = Module.create()
@@ -29,30 +31,56 @@ def construct_and_print_in_module(f: Callable[[ModuleOp], None]):
 
 
 @construct_and_print_in_module
-def test_tiling(module_: ModuleOp):
-    @func.func()
-    def payload_ir():
-        for i in scf.for_(0, 42, 1):
-            v = arith.addi(i, i)
-            # linalg.batch_matmul()
-            scf.yield_([])
-
+def test_tiling(module_: Module):
     @func.func(
-        MemRefType.get([32, 32], T.f32()),
-        MemRefType.get([32, 32], T.f32()),
-        MemRefType.get([32, 32], T.f32()),
+        tensor(32, 32, 32, element_type=T.f32()),
+        tensor(32, 32, 32, element_type=T.f32()),
+        tensor(32, 32, 32, T.f32()),
     )
-    def matmul_signed_on_buffers(lhs: Value, rhs: Value, out: Value):
-        linalg.matmul(lhs, rhs, outs=[out])
+    def matmul_signed_on_buffers(lhs: Value, rhs: Value, out: Value) -> Value:
+        return linalg.batch_matmul(lhs, rhs, outs=[out])
 
     @module(attrs={"transform.with_named_sequence": UnitAttr.get()})
     def mod():
         @named_sequence("__transform_main", [any_op_t()], [])
         def basic(target: any_op_t()):
-            m = structured_match(any_op_t(), target, ops=["arith.addi"])
-            loop = get_parent_op(pdl.op_t(), m, op_name="scf.for")
+            matmul = target.match_ops("linalg.batch_matmul")
+            loops = matmul.tile(loop=TileLoopKind.FORALL, tile_sizes=[8]).loops
+            # for loop in loops:
+            #     loop_unroll(loop, 2)
 
-            loop_unroll(loop, 2)
+    print(module_)
+
+    pm = PassManager.parse("builtin.module(transform-interpreter)")
+    pm.run(module_.operation)
+
+    print(module_)
+
+
+@construct_and_print_in_module
+def test_loop_noop(module_: Module):
+    @func.func(
+        tensor(32, 32, 32, element_type=T.f32()),
+        tensor(32, 32, 32, element_type=T.f32()),
+        tensor(32, 32, 32, T.f32()),
+    )
+    def matmul_signed_on_buffers(lhs: Value, rhs: Value, out: Value) -> Value:
+        return linalg.batch_matmul(lhs, rhs, outs=[out])
+
+    @func.func()
+    def loop_unroll_op():
+        for i in forall(0, 42, 5):
+            v = arith.addi(i, i)
+            scf.yield_([])
+
+    @module(attrs={"transform.with_named_sequence": UnitAttr.get()})
+    def mod():
+        @named_sequence("__transform_main", [any_op_t()], [])
+        def basic(target: any_op_t()):
+            matmul = target.match_ops("linalg.batch_matmul")
+            loops = matmul.tile(loop=TileLoopKind.FORALL, tile_sizes=[8]).loops
+            # for loop in loops:
+            #     loop_unroll(loop, 2)
 
     print(module_)
 
